32d94d53a912b273bbf45a7af92b3b9f1f0d168e
Aman Gupta <56467452+amangupta679@users.noreply.github.com>
2023-07-14 18:28:00 +0530

    Create MyException.java

---
+++ 14-07-2023/bookStoreProject/MyException.java
@@ -0,0 +1,18 @@
+package bookstoreProject;
+
+
+
+public class MyException extends IndexOutOfBoundsException{
+String message;
+public MyException(String message) {
+
+   
+	this.message = message;
+	
+}
+
+public String getMessage () {
+    return message;
+    
+}
+}
